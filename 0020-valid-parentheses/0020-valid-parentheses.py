425a2f3a1a55bb4cb9694de6f85492c7616f6193
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-09-06 00:40:33 +0800

    Time: 38 ms (71.85%), Space: 16.3 MB (82.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -9,15 +9,14 @@ class Solution:
         }
 
         for i in s:
-            # if i == '(' or i == '{' or i == '[':
-            if i in vaild_hash_map:
+            if i in vaild_hash_map: # push left paretheses in stack
                 stack.append(i)
             elif (len(stack) != 0) and (i == vaild_hash_map[stack[-1]]): # peek most upper element in stack
                 stack.pop()
             else:
-                return False
+                return False # parentheses not match
 
-        if len(stack) != 0:
+        if len(stack) != 0: # still some paretheses in stack, that's not we expected
             return False
 
         return True
