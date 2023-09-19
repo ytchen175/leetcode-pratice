922d96cc1d8cefe8b62dffb80e592e2a4dad6991
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-09-19 12:44:19 +0800

    Time: 44 ms (37.50%), Space: 16.6 MB (39.58%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        results = []
+        res = []
         subset = [] # declare subset as global var to prevent some annoying problem
-
+        
         def dfs(i): # index: 0, 1, 2
             if i >= len(nums):
-                results.append(subset.copy())
+                res.append(subset.copy())
                 return
             else:
-                # 不斷的把 nums[i] 加進來, 也就是左邊的 sub tree
+                # left subtree, add nums[i]
                 subset.append(nums[i])
                 dfs(i + 1)
 
-                # 不斷的不把 nums[i] 加進來, 也就是右邊的 sub tree
+                # right subtree, don't add nums[i]
                 subset.pop()
                 dfs(i + 1)
-        
-        dfs(0) # set index=0 as start point
-        return results
+
+        dfs(0) # entry point
+
+        return res
