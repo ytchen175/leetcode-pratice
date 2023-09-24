16529f34552f2a15cb4a2d941e1ab325572acac3
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-09-24 06:56:47 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- my-folder/problems/climbing_stairs/solution.py
+++ my-folder/problems/climbing_stairs/solution.py
@@ -1,20 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # stairs: 1 2 3 4...
-        # sols: 1 2 3 5...
-        prev1 = 1 # set as arr[0]
-        prev2 = 2 # set as arr[1]
-        curr = 0
+        prev1, prev2 = 1, 2 # n = 1 & n = 2 answer
+        steps = 0
         
-        sol = [prev1, prev2]
-
-        if n <= 2:
-            return n
-        else:
-            for i in range(2, n): # start DP
-                prev1, prev2 = sol[-2], sol[-1]
-                curr = prev1 + prev2
-                sol.append(curr) # save result 
-
-        return curr
-                
+        arr = [prev1, prev2] # preset known answers
+        
+        for i in range(2, n): # start DP from unknown ans
+            prev1, prev2 = arr[-1], arr[-2]
+            steps = prev1 + prev2
+            
+            arr.append(steps) # store ans
+        
+        return arr[n - 1] # get newest ans
