ab8e08a98b00b50008d2500657b7d8bf43e52134
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-09-24 14:56:54 +0800

    Time: 31 ms (91.32%), Space: 16.3 MB (59.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        prev1, prev2 = 1, 2 # n=1 & n=2 answer
-        curr = 0
+        prev1, prev2 = 1, 2 # n = 1 & n = 2 answer
+        steps = 0
         
-        arr = [prev1, prev2]
+        arr = [prev1, prev2] # preset known answers
         
-        for i in range(2, n):
+        for i in range(2, n): # start DP from unknown ans
             prev1, prev2 = arr[-1], arr[-2]
-            curr = prev1 + prev2
+            steps = prev1 + prev2
             
-            arr.append(curr)
+            arr.append(steps) # store ans
         
-        return arr[n-1]
+        return arr[n - 1] # get newest ans
