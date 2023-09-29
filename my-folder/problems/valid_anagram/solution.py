46520d88de9d7f2fc94c4a0c75c6fc97355957e1
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-09-29 16:07:10 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- my-folder/problems/valid_anagram/solution.py
+++ my-folder/problems/valid_anagram/solution.py
@@ -1,22 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        character_hash_map = dict()
-        character_hash_map1 = dict()
+        hash_map = dict()
+        hash_map1 = dict()
         
-        if len(s) != len(t):
-            return False
-        
-        else:
-            for i in s:
-                if i not in character_hash_map:
-                    character_hash_map[i] = 1
-                else:
-                    character_hash_map[i] += 1
+        for i in s:
+            if i not in hash_map:
+                hash_map[i] = 1
+            else:
+                hash_map[i] += 1
 
-            for i in t:
-                if i not in character_hash_map1:
-                    character_hash_map1[i] = 1
-                else:
-                    character_hash_map1[i] += 1
-
-        return (character_hash_map == character_hash_map1)
+        for i in t:
+            if i not in hash_map1:
+                hash_map1[i] = 1
+            else:
+                hash_map1[i] += 1
+                
+        return hash_map == hash_map1
