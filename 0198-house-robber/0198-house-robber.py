8a1e4215f29fbf68bdb91183611555f8acf4d048
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-09-30 14:40:14 +0800

    Time: 39 ms (54.94%), Space: 16.1 MB (98.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,7 +5,7 @@ class Solution:
         # [rob1, rob2, n, n+1, ...]
         for n in nums:
             local_max = max(rob1 + n, rob2) # 看搶 1位+3位 or 2位 哪個比較多錢
-            rob1 = rob2 # rob1 前進一格, eg. [rob1(x), rob1, rob2, n, ...]
+            rob1 = rob2 # rob1 前進一格, eg. [原rob1, 新rob1, 新rob2, n, ...]
             rob2 = local_max # rob2 更新為 arr 前面的最大值
         
         return rob2
