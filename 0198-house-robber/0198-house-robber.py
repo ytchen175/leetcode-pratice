ee39f9f6af19e9c86e5b4864f1269e8f7ea5a5f6
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-09-30 14:39:04 +0800

    Time: 37 ms (73.27%), Space: 16.2 MB (87.62%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        
+        # [rob1, rob2, n, n+1, ...]
+        for n in nums:
+            local_max = max(rob1 + n, rob2) # 看搶 1位+3位 or 2位 哪個比較多錢
+            rob1 = rob2 # rob1 前進一格, eg. [rob1(x), rob1, rob2, n, ...]
+            rob2 = local_max # rob2 更新為 arr 前面的最大值
+        
+        return rob2
