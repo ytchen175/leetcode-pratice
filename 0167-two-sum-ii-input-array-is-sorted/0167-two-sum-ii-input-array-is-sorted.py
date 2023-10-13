1c611fcdb659640f0a8e551b64368f3dde517074
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-10-13 14:59:59 +0800

    Time: 126 ms (23.83%), Space: 17.1 MB (76.81%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,12 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        left_ptr, right_ptr = 0, len(numbers) - 1
-
-        while (left_ptr < right_ptr):
-            two_sum = numbers[left_ptr] + numbers[right_ptr]
-
-            if two_sum == target:
-                return [left_ptr + 1, right_ptr + 1]
-            elif two_sum < target:
-                left_ptr += 1 # because numbers are sorted, we needs to find bigger one
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        
+        while (left <= right):
+            
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] > target:
+                right = right - 1
             else:
-                right_ptr -= 1 # because numbers are sorted, we needs to find smaller one
+                left = left + 1
