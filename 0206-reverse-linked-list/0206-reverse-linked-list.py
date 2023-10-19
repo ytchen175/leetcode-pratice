fb5cab18a465312e63644ca9b153cb4a46f56fbb
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-10-20 00:19:35 +0800

    Time: 35 ms (91.09%), Space: 17.7 MB (98.17%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -8,9 +8,9 @@ class Solution:
         prev, curr = None, head
 
         while (curr):
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
+            nxt = curr.next # 先把 curr 的下一個 node 暫存起來變成 nxt
+            curr.next = prev # 調換 ptr 方向，顛倒過來
+            prev = curr # 把 curr 設成 prev 
+            curr = nxt # 跳到下一個 node
 
         return prev
