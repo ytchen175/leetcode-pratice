2559b667ccb3dd72efad8f1db4e560f5ba6217cc
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-10-20 11:14:46 +0800

    Time: 42 ms (57.75%), Space: 17.8 MB (85.17%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,12 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
-
-        while (curr):
-            nxt = curr.next # 先把 curr 的下一個 node 暫存起來變成 nxt
-            curr.next = prev # 調換 ptr 方向，顛倒過來
-            prev = curr # 把 curr 設成 prev 
-            curr = nxt # 跳到下一個 node
-
+        prev, nxt = None, None
+        curr = head
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
         return prev
