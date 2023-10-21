50a8cb5485d60beaa7d0eb6fd640809ce501e2aa
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2023-10-21 15:36:27 +0800

    Time: 566 ms (26.76%), Space: 30 MB (27.45%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,13 +1,12 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         heap = []
-
-        for num in nums:
-            heapq.heappush(heap,-num) # 因為 heapq 的預設是 min heap，所以這邊存進去的時候要加負號
         
-        while k > 0:
-            res = heapq.heappop(heap) # pop 出來
-            k -=1
+        for n in nums:
+            heapq.heappush(heap, -n) # min heap
+            
+        while (k > 0):
+            res = heapq.heappop(heap)
+            k -= 1
 
-        return -res # pop 出來時要再加回去
-
+        return -res
