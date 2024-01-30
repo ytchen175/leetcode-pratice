4e60337efc4a16d13b738a6cafd2c7eb308d2e43
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2024-01-30 14:56:48 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = dict()
-        
-        for i in range(len(nums)):
-            value = nums[i]
-            need_to_find = target - value
 
-            if need_to_find in hash_map:
-                return [hash_map[need_to_find], i]
+        for i in range(0, len(nums)):
+            find = target - nums[i]
+
+            if find in hash_map:
+                return [i, hash_map[find]]
             else:
-                hash_map[value] = i
-                
-                
+                hash_map[nums[i]] = i
