2c7b193d455b3157f9d5f813b415a0537d1d9c2b
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2024-02-04 07:04:16 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- my-folder/problems/top_k_frequent_elements/solution.py
+++ my-folder/problems/top_k_frequent_elements/solution.py
@@ -1,17 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_dict = dict()
-        res = []
+        counts = collections.Counter(nums)
+
+        return heapq.nlargest(k, iterable=counts.keys(), key=counts.get)
         
-        for n in nums:
-            if n not in count_dict:
-                count_dict[n] = 1
-            else:
-                count_dict[n] += 1
-
-        sorted_count_dict = dict(
-            sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
-        )
-        res = list(sorted_count_dict.keys())
-
-        return res[:k]
