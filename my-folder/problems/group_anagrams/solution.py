ced330977aeea208c96bc1b444858012300fa109
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2024-02-04 06:17:52 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- my-folder/problems/group_anagrams/solution.py
+++ my-folder/problems/group_anagrams/solution.py
@@ -1,33 +1,13 @@
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         res = dict()
-#         hash_map = dict()
-
-#         # {'abc': ['abc', 'cba']}
-#         for s in strs:
-#             d = collections.Counter(s)
-#             found = False
-
-#             for k in res.keys():
-#                 if collections.Counter(k) == d:
-#                     res[k].append(s)
-#                     found = True
-#                     break
-
-#             if not found:
-#                 res[s] = [s]
-
-#         return res.values()
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)  # Use defaultdict to avoid key errors
+        d = dict()
         
         for s in strs:
-            # Sort the string and use it as a key
             sorted_str = ''.join(sorted(s))
-            anagrams[sorted_str].append(s)
-        
-        # No need to use .values() in list comprehension, it's already a list of lists
-        return list(anagrams.values())
+
+            if sorted_str not in d:
+                d[sorted_str] = [s]
+            else:
+                d[sorted_str].append(s)
+
+        return d.values()
