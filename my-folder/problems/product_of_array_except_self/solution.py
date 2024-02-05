a57a553bfe0f203b168570f99c71e32c421240db
ytchen175 <58975450+ytchen175@users.noreply.github.com>
2024-02-05 15:42:38 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- my-folder/problems/product_of_array_except_self/solution.py
+++ my-folder/problems/product_of_array_except_self/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         length = len(nums)
-        output = [1] * length
+        from_left, from_right = [1] * length, [1] * length
         
-        # Calculate products of all elements to the left of each index
         left_product = 1
         for i in range(length):
-            output[i] = left_product
+            from_left[i] = left_product
             left_product *= nums[i]
-        
-        # Calculate products of all elements to the right of each index
-        # and multiply with the left products
+
         right_product = 1
-        for i in reversed(range(length)):
-            output[i] *= right_product
+        for i in range(length - 1, -1, -1):
+            from_right[i] = right_product
             right_product *= nums[i]
         
-        return output
+        res = [i * j for i, j in zip(from_left, from_right)]
+        return res
